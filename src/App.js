77914cda02179d533978efc49ef794ed6149a38a
MaistroUwei <0208735@up.edu.mx>
2022-09-19 20:41:27 -0500

    jelp

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     apellido: "",
     apodo: ""
   });
+  const botonActivo = (cliente.nombre !== undefined && cliente.nombre !=="") && (cliente.apellido !== undefined && cliente.apellido !=="") && (cliente.apodo !== undefined && cliente.apodo !=="");
+  const [formularioEnv, setFormEnv] = useState(false);
 
   const manejarForm = (event) => {
     const {name, value}  = event.target;
@@ -15,22 +17,40 @@ function App() {
       [name]: value
     })) 
   }
+
+  const enviar = (event) => {
+    event.preventDefault();
+    setFormEnv(true);
+    // estado
+  }
+
+  const limpiar = (event) =>{
+    setCliente({nombre:"",apellido:"",apodo:""})
+  }
+
   return (
     <div className="App">
-      <form>
-        <div>
-          <label htmlFor=''>Nombre:</label>
-          <input type="text" value={cliente.nombre} name="nombre" onChange={manejarForm}/>
-        </div>
-        <div>
-          <label htmlFor=''>Apellido:</label>
-          <input type="text" value={cliente.apellido} name="apellido" onChange={manejarForm}/>
-        </div>
-        <div>
-          <label htmlFor=''>Apodo:</label>
-          <input type="text" value={cliente.apodo} name="apodo" onChange={manejarForm}/>
-        </div>
-      </form>
+      <main>
+        <form onSubmit={enviar}>
+          <div>
+            <label htmlFor=''>Nombre:</label>
+            <input type="text" value={cliente.nombre} name="nombre" onChange={manejarForm}/>
+          </div>
+          <div>
+            <label htmlFor=''>Apellido:</label>
+            <input type="text" value={cliente.apellido} name="apellido" onChange={manejarForm}/>
+          </div>
+          <div>
+            <label htmlFor=''>Apodo:</label>
+            <input type="text" value={cliente.apodo} name="apodo" onChange={manejarForm}/>
+          </div>
+          <button type = "submit" disabled={!botonActivo}>Enviar</button>
+          <button onClick={limpiar}>Limpiar</button>
+        </form>
+        {formularioEnv && (
+          <p>Datos: {`${cliente.nombre}`} {`${cliente.apellido}`} {`${cliente.apodo}`}</p>
+        )}
+      </main>
     </div>
   );
 }
